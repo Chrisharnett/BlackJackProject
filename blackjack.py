7ab01a8882873ad9c1c856c913c84d15bb7f8358
Bard of Code <harnettmusic@gmail.com>
2023-03-21 15:36:52 -0230

    BlackJack Project for Python

--- blackjack.py
+++ blackjack.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env/python3
-def deck():
+
+import db
+import random
+
+
+def newDeck():
     suits = ["clubs", "spades", "hearts", "diamonds"]
     numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
     deck = []
     for suit in suits:
         for number in numbers:
-            if number == "A":
-                value = 1
+            if number.upper() == "A":
+                value = "1"
             if number == "J" or number == "Q" or number == "K":
-                value = 10
+                value = "10"
             else:
-                value = int(number)
-            deck.append(number, suit, value)
+                value = number
+            deck.append([number, suit, value])
+        random.shuffle(deck)
     return deck
 
 
-
-
 def main():
-    pass
+    print("BlackJack Project")
+    money = db.load_money_from_disk()
+    deck = newDeck()
 
 
 if __name__ == '__main__':
-    main
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
