e6fa1a04a496cf68b73d4465910ab4c87ec0787c
Bard of Code <harnettmusic@gmail.com>
2023-03-21 15:12:25 -0230

    setting up blackjack project

--- blackjack.py
+++ blackjack.py
@@ -2,11 +2,16 @@
 def deck():
     suits = ["clubs", "spades", "hearts", "diamonds"]
     numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    deck = []
     for suit in suits:
         for number in numbers:
             if number == "A":
                 value = 1
-            if number == "J" or number == "Q"
+            if number == "J" or number == "Q" or number == "K"
+                value = 10
+            else:
+                value = int(number)
+
 
 
 def main():
