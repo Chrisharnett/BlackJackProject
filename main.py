69fbebf1d84ef8d83c7e6f4bb4119dcf0c3b0ff0
Bard of Code <harnettmusic@gmail.com>
2023-03-21 15:03:05 -0230

    BlackJack Project for Python

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env/python3
 def deck():
-    pass
+    suits = ["clubs", "spades", "hearts", "diamonds"]
+    number = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    deck = []
+
 
 def main():
     pass
