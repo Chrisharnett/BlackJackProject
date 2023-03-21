578a36d282d292be0b00ba9bc95c0af69b78e4ae
Bard of Code <harnettmusic@gmail.com>
2023-03-21 15:08:16 -0230

    BlackJack Project for Python

---
+++ blackjack.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env/python3
+def deck():
+    suits = ["clubs", "spades", "hearts", "diamonds"]
+    numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    for suit in suits:
+        for number in numbers:
+            if number == "A":
+                value = 1
+            if number == "J" or number == "Q"
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
